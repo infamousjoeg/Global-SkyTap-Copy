5e7b01d55bb988d47b9b01460bd9cbe9131e4d16
Joe Garcia, CISSP <joe@joe-garcia.com>
2017-10-20 12:30:04 -0400

    Updated

--- README.md
+++ README.md
@@ -8,7 +8,10 @@ Other than that... this is a work in progress.
 
 ## Template Copy Process Mapping
 
-For my sanity and your non-need to repeat yourself to me
+For my sanity and your non-need to repeat yourself to me:
 
 1. Copy template **US CyberArk Global Demo `{{env}}`** to new region.  (For this example, `EMEA`)
-2. This creates the template named **Copy of US CyberArk Global Demo
+1. This creates the template named **Copy of US CyberArk Global Demo `{{env}}`**.
+1. The new template should be renamed to **EMEA CyberArk Global Demo `{{env}}`**.
+1. New template needs to be added to the Project **EMEA CyberArk Global Demo `{{env}}`**.
+1. Change owner to whoever Len says but for now, just Len.
--- copy.ps1
+++ copy.ps1
@@ -1,59 +1,4 @@
-function Set-SkyTapAuth ([string]$username, [string]$password) {
-    
-    # Base64 Encode username:api-key for Authentication header
-    $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username,$password)))
-
-    $base64AuthInfo
-}
-
-function Set-TemplateCopy ([string]$basic_auth, [int]$template_id, [string]$target_region, [bool]$copy_override=$true) {
-    
-    # Set URI endpoint to send call to
-    $uri = "https://cloud.skytap.com/templates.json"
-
-    # Set headers required for authentication and format
-    $headers = @{"Accept" = "application/json"; Authorization=("Basic {0}" -f $basic_auth)}
-
-    $body = @{
-        "template_id" = $template_id
-        "target_region" = $target_region
-        "copy_to_region_vm_status_override" = $copy_override
-    } | ConvertTo-Json
-
-    # Send the API call
-    Invoke-RestMethod -Uri $uri -Method POST -Body $body -ContentType "application/json" -Headers $headers
-
-}
-
-function Set-TemplateOwner ([string]$basic_auth, [int]$template_id, [string]$target_owner) {
-    
-    # Set URI endpoint to send call to
-    $uri = "https://cloud.skytap.com/${template_id}.json"
-
-    # Set headers required for authentication and format
-    $headers = @{"Accept" = "application/json"; Authorization=("Basic {0}" -f $basic_auth)}
-
-    $body = @{
-        "owner" = "${target_owner}"
-    } | ConvertTo-Json
-
-    # Send the API call
-    Invoke-RestMethod -Uri $uri -Method PUT -Body $body -ContentType "application/json" -Headers $headers
-
-}
-
-function Set-TemplateName ([string]$basic_auth, [int]$template_id, [string]$new_name) {
-
-    # Set URI endpoint to send call to
-    $uri = "https://cloud.skytap.com/template/${template_id}?name=${new_name}"
-
-    # Set headers required for authentication and format
-    $headers = @{"Accept" = "application/json"; Authorization=("Basic {0}" -f $basic_auth)}
-
-    # Send the API call
-    Invoke-RestMethod -Uri $uri -Method PUT -ContentType "application/json" -Headers $headers
-
-}
+Import-Module "SkyTapAPI.psm1" | Out-Null
 
 Write-Host "================================================================" -ForegroundColor Yellow
 Write-Host "================================================================" -ForegroundColor Yellow
@@ -87,6 +32,9 @@ foreach ($region in $ConfigFile.Settings.Template.Regions) {
     
     foreach ($env in $ConfigFile.Settings.Template.Environments) {
 
+        # Set error flag to null for "No error"
+        $errFlag = $null
+
         # Begin copy template process
         $responseCopyTemplate = Set-TemplateCopy -basic_auth $base64AuthInfo -template_id $ConfigFile.Settings.Template.CopyID -target_region $region
 
@@ -103,22 +51,32 @@ foreach ($region in $ConfigFile.Settings.Template.Regions) {
                 Write-Host "[ Step 1/3 ] Template successfully queued for copy" -ForegroundColor Cyan
             } else {
                 Write-Host "An error occurred while attempting to copy the template via API." -ForegroundColor Red
+                $errFlag = 1
             }
 
             # Begin owner change process
             $responseChangeOwner = Set-TemplateOwner -basic_auth $base64AuthInfo -template_id $responseCopyId -target_owner $ConfigFile.Settings.Template.NewOwnerID
 
-            if ($responseChangeOwner) {
+            if ($responseChangeOwner -and !$errFlag) {
                 Write-Host "[ Step 2/3 ] Owner successfully updated to ${ConfigFile.Settings.Template.NewOwnerID}" -ForegroundColor Cyan
             } else {
                 Write-Host "An error occurred while attempting to change the owner via API." -ForegroundColor Red
+                $errFlag = 1
             }
 
             # Begin name change process
             $copyNameBase = $responseCopyName.TrimStart("US ")
             $copyNameBase = $copyNameBase.TrimEnd(" ${env} - Copy")
             $copyNewName = "${region} - ${copyNameBase} ${env}"
-            $responseChangeName = Set-TemplateName -basic_auth $base64AuthInfo -template_id $responseCopyId -new_name
+            
+            $responseChangeName = Set-TemplateName -basic_auth $base64AuthInfo -template_id $responseCopyId -new_name $copyNewName
+
+            if ($responseChangeName -and !$errFlag) {
+                Write-Host "[ Step 3/3 ] Name of template successfully updated to ${copyNewName}" -ForegroundColor Cyan
+            } else {
+                Write-Host "An error occurred while attempting to change the name of the template via API." -ForegroundColor Red
+                $errFlag = 1
+            }
 
         } else {
             Write-Host "No response was received from the API call." -ForegroundColor Red
