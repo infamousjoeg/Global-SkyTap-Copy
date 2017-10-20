f0678a3579f9f15147b2e57f8962467b7f21bc40
Joe Garcia <joe.garcia@cyberark.com>
2017-10-20 14:22:18 -0400

    Release v0.1

--- copy.ps1
+++ copy.ps1
@@ -1,4 +1,4 @@
-Import-Module "SkyTapAPI.psm1" | Out-Null
+Import-Module "${pwd}\SkyTapAPI.psm1" | Out-Null
 
 Write-Host "================================================================" -ForegroundColor Yellow
 Write-Host "================================================================" -ForegroundColor Yellow
@@ -28,8 +28,10 @@ Write-Host " "
 # Base64 Encode username:api-key for Authentication header
 $base64AuthInfo = Set-SkyTapAuth -username $ConfigFile.Settings.Authentication.SkyTapAPIUser -password $ConfigFile.Settings.Authentication.SkyTapAPIPass
 
+# Loop through each <Region> in config.xml
 foreach ($region in $ConfigFile.Settings.Template.Regions) {
     
+    # Loop through each <Environment> in config.xml
     foreach ($env in $ConfigFile.Settings.Template.Environments) {
 
         # Set error flag to null for "No error"
@@ -45,10 +47,9 @@ foreach ($region in $ConfigFile.Settings.Template.Regions) {
             # Set variables based on response values
             $responseCopyId = $responseCopyTemplate.id
             $responseCopyName = $responseCopyTemplate.name
-            $responseCopyRegion = $responseCopyTemplate.region
 
             if ($responseCopyId) {
-                Write-Host "[ Step 1/3 ] Template successfully queued for copy" -ForegroundColor Cyan
+                Write-Host "[ Step 1/4 ] Template successfully queued for copy" -ForegroundColor Cyan
             } else {
                 Write-Host "An error occurred while attempting to copy the template via API." -ForegroundColor Red
                 $errFlag = 1
@@ -58,7 +59,7 @@ foreach ($region in $ConfigFile.Settings.Template.Regions) {
             $responseChangeOwner = Set-TemplateOwner -basic_auth $base64AuthInfo -template_id $responseCopyId -target_owner $ConfigFile.Settings.Template.NewOwnerID
 
             if ($responseChangeOwner -and !$errFlag) {
-                Write-Host "[ Step 2/3 ] Owner successfully updated to ${ConfigFile.Settings.Template.NewOwnerID}" -ForegroundColor Cyan
+                Write-Host "[ Step 2/4 ] Owner successfully updated to ${ConfigFile.Settings.Template.NewOwnerID}" -ForegroundColor Cyan
             } else {
                 Write-Host "An error occurred while attempting to change the owner via API." -ForegroundColor Red
                 $errFlag = 1
@@ -66,15 +67,28 @@ foreach ($region in $ConfigFile.Settings.Template.Regions) {
 
             # Begin name change process
             $copyNameBase = $responseCopyName.TrimStart("US ")
-            $copyNameBase = $copyNameBase.TrimEnd(" ${env} - Copy")
-            $copyNewName = "${region} - ${copyNameBase} ${env}"
+            $copyNameBase = $copyNameBase.TrimEnd(" - Copy")
+            $copyNewName = "${region} ${copyNameBase}"
             
             $responseChangeName = Set-TemplateName -basic_auth $base64AuthInfo -template_id $responseCopyId -new_name $copyNewName
 
             if ($responseChangeName -and !$errFlag) {
-                Write-Host "[ Step 3/3 ] Name of template successfully updated to ${copyNewName}" -ForegroundColor Cyan
+                Write-Host "[ Step 3/4 ] Name of template successfully updated to ${copyNewName}" -ForegroundColor Cyan
             } else {
                 Write-Host "An error occurred while attempting to change the name of the template via API." -ForegroundColor Red
+                $errFlag = 1
+            }
+
+            # Set Project Tag for pulling ID from config.xml
+            $projectTag = "${region}${env}"
+            
+            # Add template to proper project for distribution
+            $responseChangeProject = Set-TemplateProject -basic_auth $base64AuthInfo -template_id $responseCopyId -project_id $ConfigFile.Settings.Template.Projects.${projectTag}
+
+            if ($responseChangeProject -and !$errFlag) {
+                Write-Host "[ Step 4/4 ] Template successfully added to the ${region} CyberArk Global Demo ${env} Project" -ForegroundColor Cyan
+            } else {
+                Write-Host "An error occurred while attempting to add the template to a project via API." -ForegroundColor Red
                 $errFlag = 1
             }
 
