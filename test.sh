6939077a07a036c7c66e98ec137d402749b23433
Joe Garcia, CISSP <infamousjoeg@users.noreply.github.com>
2018-02-14 18:14:58 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -eo pipefail
 
-summon docker run --name gsc_test -d -e 'SKYTAP_USER=$SKYTAP_USER' -e 'SKYTAP_PASS=$SKYTAP_PASS' -e 'SKYTAP_REGION=$SKYTAP_REGION' nfmsjoeg/gsc:test
+summon docker run --name gsc_test -d -e 'SKYTAP_USER=$SKYTAP_USER' -e 'SKYTAP_PASS=$SKYTAP_PASS' nfmsjoeg/gsc:test
