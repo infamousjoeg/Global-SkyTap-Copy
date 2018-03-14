e2cdd05535a6b73342195f1a1bacc6da543938de
Joe Garcia, CISSP <joe.garcia@cyberark.com>
2018-03-14 11:44:00 -0400

    Initial Release :confetti_ball:

--- gsc.ps1
+++ gsc.ps1
@@ -8,14 +8,18 @@ if (!$(Get-Module PoShSkyTap).Version) {
 [xml]$ConfigFile = Get-Content -Path "/etc/gsc/skytap.cred.xml"
 $authSkyTap = Set-SkyTapAuth -Username $ConfigFile.SkyTap.Credentials.Username -APIKey $ConfigFile.SkyTap.Credentials.Password
 
+# User Input
+Write-Host "--------------------------" - -ForegroundColor Yellow
 $masterTemplateID = Read-Host "Please enter the master Template's ID"
 $targetRegion = Read-Host "Please enter region to copy to [ US-East | EMEA* | APAC ]"
 if (!$targetRegion) { $targetRegion = "EMEA" }
 elseif ($targetRegion -ne "US-East" -and $targetRegion -ne "EMEA" -and $targetRegion -ne "APAC") { Write-Host "Choose correctly."; exit }
-$targetEnv = Read-Host "Please enter the environment to copy to [ DEV | QA | GA* ]"
-if (!$targetEnv) { $targetEnv = "GA" }
+$targetEnv = Read-Host "Please enter the environment to copy to [ DEV | QA* | GA ]"
+if (!$targetEnv) { $targetEnv = "QA" }
+elseif ($targetEnv -ne "GA" -and $targetEnv -ne "DEV" -and $targetEnv -ne "QA") { Write-Host "Choose correctly."; exit }
 
-$r_TemplateCopy = Set-TemplateCopy -SkyTapAuth $authSkyTap -TemplateID $masterTemplateID -TargetRegion $targetRegion | ConvertFrom-Json
+# Start Template Copy
+$r_TemplateCopy = Set-TemplateCopy -SkyTapAuth $authSkyTap -TemplateID $masterTemplateID -TargetRegion $targetRegion
 
 $copyTemplateID = $r_TemplateCopy.id
 
@@ -23,24 +27,26 @@ if (!$copyTemplateID) { Write-Host "Error occured trying to start copy."; exit}
 
 Write-Host "Copy process started.  New Template ID: ${copyTemplateID}" - -ForegroundColor Green
 
-$r_TemplateName = Set-TemplateName -SkyTapAuth $authSkyTap -TemplateID $copyTemplateID -TemplateName "${targetRegion} CyberArk Global Demo v10_${targetEnv}" | ConvertFrom-Json
+# Set Template Name
+$r_TemplateName = Set-TemplateName -SkyTapAuth $authSkyTap -TemplateID $copyTemplateID -TemplateName "${targetRegion} CyberArk Global Demo v10_${targetEnv}"
 
 $copyTemplateName = $r_TemplateName.name
 
-if ($copyTemplateName) { Write-Host "Set template name to: ${copyTemplateName" -ForegroundColor Green }" }
+if ($copyTemplateName) { Write-Host "Set template name to: ${copyTemplateName}" -ForegroundColor Green }
 else { Write-Host "Template name not set correctly."; exit }
 
+# Set Template Owner
 switch ($targetRegion) {
     "US-East" { $ownerID = "279622" }
     "EMEA" { $ownerID = "279620" }
     "APAC" { $ownerID = "279618" }
 }
 
-$r_TemplateOwner = Set-TemplateOwner -SkyTapAuth $authSkyTap -TemplateID $copyTemplateID -OwnerID $ownerID | ConvertFrom-Json
+$r_TemplateOwner = Set-TemplateOwner -SkyTapAuth $authSkyTap -TemplateID $copyTemplateID -OwnerID $ownerID
 
-if ($r_TemplateOwner.owner -ne $ownerID) { Write-Host "Error occurred assigning ownership."; exit }
+$copyOwnerID = $r_TemplateOwner.owner
 
-Write-Host "Set Owner to: ${ownerID}" -ForegroundColor Green
+Write-Host "Set Owner to: ${copyOwnerID}" -ForegroundColor Green
 
 switch ($targetRegion) {
     "US-East" {
@@ -66,10 +72,14 @@ switch ($targetRegion) {
     }
 }
 
-$r_TemplateProject = Set-TemplateProject -SkyTapAuth $authSkyTap -TemplateID $copyTemplateID -ProjectID $projectID | ConvertFrom-Json
+$r_TemplateProject = Set-TemplateProject -SkyTapAuth $authSkyTap -TemplateID $copyTemplateID -ProjectID $projectID
 
 $projectName = $r_TemplateProject.name 
 
 Write-Host "Added template to project: ${projectName}" -ForegroundColor Green
+
+Write-Host "--------------------------" -ForegroundColor Yellow
+
+Write-Host "New template can be found at: https://cloud.skytap.com/templates/${copyTemplateID}" -ForegroundColor Cyan
 
 Write-Host "Script complete." -ForegroundColor Yellow
