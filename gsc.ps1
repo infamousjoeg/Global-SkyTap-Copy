88c566e129331bd2009ec6d86dcbc9b8b59e3e33
Joe Garcia, CISSP <joe.garcia@cyberark.com>
2018-03-14 09:58:17 -0400

    Testing Input to Docker Container

--- gsc.ps1
+++ gsc.ps1
@@ -8,3 +8,8 @@ if ($chkPSModule.Version) {
 } else {
     Write-Host "PoShSkyTap was not installed successfully." -ForegroundColor Red
 }
+
+#[xml]ConfigFile = Get-Content -Path "skytap.cred.xml"
+#$authSkyTap = Set-SkyTapAuth -Username $ConfigFile.SkyTap.Credentials.Username -APIKey $ConfigFile.SkyTap.Credentials.Password
+
+Write-Host $(Read-Host "Enter any input for return")
